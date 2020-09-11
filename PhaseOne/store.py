2722e5fc1d0a6feb5a5768f9fb06fb47fd07dca8
isaiah-barron <isaiahdbarron@gmail.com>
2020-09-11 15:57:34 -0700

    Finished insert() and PUT requests
    I am able to insert new or existing key/value into my key/value store with PUT requests. Edge cases are that the key is too long in the PUT request or there is no value paired with the key in the PUT request

--- PhaseOne/store.py
+++ PhaseOne/store.py
@@ -7,8 +7,19 @@ class Store():
 	kv_store = {}
 
 	def insert(self, key, val):
-		Store.kv_store['key'] = val
-		return json.dumps({"message":"Added successfully","replaced":False}) + '\n', status.HTTP_201_CREATED
+		update = False
+		if key in Store.kv_store:
+			update = True
+
+		#insert key/value parir into store
+		#will create a new entry or update existing key
+		Store.kv_store[key] = val
+
+		#to indicate which response to return to client 
+		if update:
+			return json.dumps({"message":"Updated successfully","replaced":True}) + '\n', status.HTTP_200_OK
+		else:
+			return json.dumps({"message":"Added successfully","replaced":False}) + '\n', status.HTTP_201_CREATED
 
 	# def get(key):
 
