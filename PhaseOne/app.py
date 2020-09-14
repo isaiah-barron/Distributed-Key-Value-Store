6bcda1fbd659b266795d26d2b68c64883ceaf9ba
isaiah-barron <isaiahdbarron@gmail.com>
2020-09-14 15:19:27 -0700

    updated app.py for better readability and maintanability

--- PhaseOne/app.py
+++ PhaseOne/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_api import status
-import json
 from store import Store
+import json
 
 app = Flask(__name__)
 myStore = Store()
@@ -10,20 +10,29 @@ myStore = Store()
 def store(key):
 
 	if request.method == 'PUT':
-		json_data = request.get_json()
-		if json_data:
-			val = json_data['value']
-			if len(key) > 16:
-				return json.dumps({"error":"Key is too long","message":"Error in PUT"}) +'\n', status.HTTP_400_BAD_REQUEST
-			else:
-				return myStore.insert(key, val)
-		else:
-			return json.dumps({"error":"Value is missing","message":"Error in PUT"}) + '\n', status.HTTP_400_BAD_REQUEST
+		return putRequest(key)
 	elif request.mehtod == 'GET':
-		return myStore.get(key)
-	#else if DELETE request
-		#if key exists, delete key and value from store, return 200 status code
-		#else return 404 error code
+		return getRequest(key)
+	#elif request.mehtod == 'DELETE':
+
+
+def putRequest(key):
+	json_data = request.get_json()
+	if json_data:
+		val = json_data['value']
+		if len(key) > 16:
+			return json.dumps({"error":"Key is too long","message":"Error in PUT"}) +'\n', status.HTTP_400_BAD_REQUEST
+		else:
+			return myStore.insert(key, val)
+	else:
+		return json.dumps({"error":"Value is missing","message":"Error in PUT"}) + '\n', status.HTTP_400_BAD_REQUEST
+
+def getRequest(key):
+	return myStore.get(key)
+
+# def deleteRequest(key):
+	#if key exists, delete key and value from store, return 200 status code
+	#else return 404 error code
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8081)
