0275a464276d4fc9feabcf8e8927c06b85e8fcb1
isaiah-barron <isaiahdbarron@gmail.com>
2020-09-23 19:41:40 -0700

    updated app.py
    added functionality for Main and Follower instance

--- PhaseOne/app.py
+++ PhaseOne/app.py
@@ -1,22 +1,49 @@
 from flask import Flask, request
 from flask_api import status
 from store import Store
+import requests
 import json
+import os
 
 app = Flask(__name__)
 myStore = Store()
 
 @app.route('/kv-store/<key>', methods=['GET', 'PUT', 'DELETE'])
 def store(key):
-	if request.method == 'PUT':
-		return putRequest(key)
-	elif request.method == 'GET':
-		return getRequest(key)
-	elif request.method == 'DELETE':
-		return deleteRequest(key)
+	forwarding = os.environ.get('FORWARDING_ADDRESS')
 
+	if forwarding == 'not_set': #client directly sent the request to the main instance
+		if request.method == 'PUT':
+			return putRequest(key)
+		elif request.method == 'GET':
+			return getRequest(key)
+		elif request.method == 'DELETE':
+			return deleteRequest(key)
+	
+	else: #client directly sent the request to the follower instance
+		main_url = "http://" + forwarding + "/kv-store/" + key
+
+		#forward the request to the main instance
+		try:
+			resp = requests.request(method=request.method,
+									url=main_url, 
+									headers=request.headers,
+									data=request.data, timeout=1)
+		except requests.exceptions.Timeout:
+			if request.method == 'GET':
+				err = json.dumps({"error":"Main instance is down","message":"Error in GET"})
+			elif request.method == 'PUT':
+				err = json.dumps({"error":"Main instance is down","message":"Error in PUT"})
+			elif request.method == 'DELETE':
+				err = err = json.dumps({"error":"Main instance is down","message":"Error in DELETE"})
+
+			return err + '\n', status.HTTP_503_SERVICE_UNAVAILABLE
+		else:
+			return resp.content, resp.status_code
+		
 def putRequest(key):
 	json_data = request.get_json()
+
 	if json_data:
 		val = json_data['value']
 		if len(key) > 16:
@@ -33,4 +60,4 @@ def deleteRequest(key):
 	return myStore.delete(key)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8081)
+	app.run(host='0.0.0.0', port=13800)
